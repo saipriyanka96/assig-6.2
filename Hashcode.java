dc1ab3945191e91d31601bad042e12e12ef75228
saipriyanka96 <31105187+saipriyanka96@users.noreply.github.com>
2017-09-03 18:05:12 +0530

    Update Hashcode.java

--- Hashcode.java
+++ Hashcode.java
@@ -4,6 +4,7 @@ import hashcode.Word;
 //import is used to make classes and interfaces available and accessible to the current 
 //source code
 public class Hashcode {
+	//Hashcode which digests the data stored in instance of the class into single hash value
 	//public keyword is used in the declaration of a class,method or field;public classes,method and fields can be accessed by the members of any class.
 	//class defines instance and class fields,methods and inner classes as well as specifying the interfaces the class implements and the immediate superclass of the class
 
